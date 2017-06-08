1c9c6d621521541126a722b73d1f85efb183e93e
mortad <qaism474@gmail.com>
2017-06-08 21:44:26 +0300

    Create config.lua

--- data/config.lua
+++ data/config.lua
@@ -1,5 +1,5 @@
 do local _ = {
-  about_text = "Welcome to DevPoint For more information Subscribe to the channel @DevPointTeam",
+  about_text = "Welcome to DevPoint For more information Subscribe to the channel @pompm",
   enabled_plugins = {
     "admin",
     "onservice",
